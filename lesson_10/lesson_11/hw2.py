392e0f1eb31a9268ce4708f16d240ab7d7baa89a
mehrishka177 <mehri@hajiagali.com>
2024-09-18 20:44:28 +0400

    loops

---
+++ lesson_10/lesson_11/hw2.py
@@ -0,0 +1,14 @@
+
+print("please enter your birthday date")
+friend_1 = "2 november"
+friend_2 = "14 november"
+friend_3 = "25 september"
+friend_4 = "9 january"
+friend_5 = "6 jully"
+print("friend 1:", friend_1)
+print("friend 2:", friend_2)
+print("friend 3:", friend_3)
+print("friend 4:", friend_4)
+print("friend 5:", friend_5)
+
+
