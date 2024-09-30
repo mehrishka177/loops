a40d3371776b54f1ab6f73b58029778bcdf20be7
mehrishka177 <mehri@hajiagali.com>
2024-09-30 17:47:23 +0400

    loops

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/a1.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+pologon = turtle.Turtule()
+
+num_slides = 6
+side_length = 70
+angle = 360.0 / num_slides
+for i in range(num_sides):
+    polygon.forward(side_length)
+    pologon.right(angle)
+
+turtle.done()
