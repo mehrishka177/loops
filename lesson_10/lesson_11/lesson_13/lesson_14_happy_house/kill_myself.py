ff15266d421f9aeb1a1066079c5ebf6b65262892
mehrishka177 <mehri@hajiagali.com>
2024-09-30 18:13:19 +0400

    loops

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/kill_myself.py
@@ -0,0 +1,29 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(100)
+
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+turtle.done()
