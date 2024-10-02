bcf597be0faea68b0ae8237ec7eddc82cca5e775
mehrishka177 <mehri@hajiagali.com>
2024-10-02 20:20:55 +0400

    loops

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/happy.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print('The weather is pleasant in: ', spring)
+    print('The weather is same in', autumn)
+    
+spring = "autumn"
+autumn = spring
+weather_condition()
