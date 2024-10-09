af5324bf8812adbed8e64001cd943771c673eb4d
mehrishka177 <mehri@hajiagali.com>
2024-10-09 20:14:29 +0400

    loops

--- lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/lesson_16_heh-heh/yyyuuyuyuyuuuyuyuy.py
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/lesson_16_heh-heh/yyyuuyuyuyuuuyuyuy.py
@@ -1,12 +1,11 @@
 def factorial(x):
 
 
-   'if x==0 or x==1'
+   if x==0 or x==1:
        return 1
    else:
        return x*factorial(x-1)
    
-print(factorial.__doc__)
 print("the factorial of 0:", factorial(0))
 print("the factorial of 1:", factorial(1))
 print("the factorial of 2:", factorial(4))
