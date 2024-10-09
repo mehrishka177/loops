e670ca6c72d7a8e8f48f7c18895db57e85ed3600
mehrishka177 <mehri@hajiagali.com>
2024-10-09 20:34:34 +0400

    loops

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/lesson_16_heh-heh/lesson_teachind_miss_Sudha/eyyaaaattrtttttr.py
@@ -0,0 +1,19 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("twist")
+    
+    elif x % 15 == 0:
+        pass
+    
+      
+    elif x % 5 == 0:
+        print("fizz")
+    
+    
+      
+    elif x % 3 == 0:
+        print("buzz")
+        
+    else:
+        print(x)
+    
