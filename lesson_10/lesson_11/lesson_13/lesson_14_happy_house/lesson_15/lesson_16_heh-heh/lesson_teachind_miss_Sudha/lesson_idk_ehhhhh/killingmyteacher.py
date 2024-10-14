e64e7aa6f1a703d20fa80391e362501aba39687e
mehrishka177 <mehri@hajiagali.com>
2024-10-14 20:52:49 +0400

    kwjdn

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/lesson_16_heh-heh/lesson_teachind_miss_Sudha/lesson_idk_ehhhhh/killingmyteacher.py
@@ -0,0 +1,19 @@
+from ctypes import byref
+from turtle import bye
+
+
+print("bye
+      pygame.Surface.get_bytesize()\byebye
+      byebye
+      byebye
+      byebye
+      byebye
+      byebye
+      byebye
+      byebyebye
+          bye
+          byebye
+          byebye
+          byebye
+          byebye
+          byebye")
