3bcac2e9fdc37512cc452435a82d3fccfb1ca692
mehrishka177 <mehri@hajiagali.com>
2024-10-14 20:43:48 +0400

    iulergsh

---
+++ lesson_10/lesson_11/lesson_13/lesson_14_happy_house/lesson_15/lesson_16_heh-heh/lesson_teachind_miss_Sudha/lesson_idk_ehhhhh/hahahah.py
@@ -0,0 +1,18 @@
+num1, num2 = eval(inpput("enter numbers nevermind random"))
+result = num1 / num2
+print("Result is", result)
+
+except ZeroDivisionError:
+    print("division eroorrr")
+    
+except SyntaxError:
+    print("wre is coma yall")
+    
+except:
+    print("are you dump")
+    
+finally:
+    print("ex no matter  wha")
+
+
+    
